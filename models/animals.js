9f8df4ee0d1529a524522cca3f228ebc0d7813df
undefined <hollisjamison@gmail.com>
2021-08-12 19:02:20 -0500

    Removes non pug files

--- models/animals.js
+++
@@ -1,9 +0,0 @@
-const Animals = (connection, Sequelize, Departments) => connection.define('animals', {
-  id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-  name: { type: Sequelize.STRING, allowNull: false },
-  species: { type: Sequelize.STRING, allowNull: false },
-  protected: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false },
-  departmentId: { type: Sequelize.INTEGER, references: { model: Departments, key: 'id' } },
-}, { paranoid: true })
-
-module.exports = Animals
--- models/departments.js
+++
@@ -1,7 +0,0 @@
-const Departments = (connection, Sequelize) => connection.define('departments', {
-  id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-  name: { type: Sequelize.STRING, allowNull: false, unique: true },
-  protected: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false },
-}, { paranoid: true })
-
-module.exports = Departments
